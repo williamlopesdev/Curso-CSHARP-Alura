e7ef0f6f6a618c61c5ca87de2deb769ff1993cbf
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-10-10 20:50:55 -0300

    Reestrutura da pasta

---
+++ Orientacao a Objetos/ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            contaDoBruno.Depositar(500);
+            Console.Write(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela " + contaDaGabriela.saldo);
+
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela " + contaDaGabriela.saldo);
+
+            Console.WriteLine("Resultado Transferencia: " + resultadoTransferencia);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ Orientacao a Objetos/ByteBank/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,69 @@
+﻿//using _05_ByteBank;
+
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+        }
+                    
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+
+            }
+            else
+            {
+                this._saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+
+
+    }
+}
